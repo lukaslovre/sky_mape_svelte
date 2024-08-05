81aa344c23a8f0bc868c93dce6c1cdb232ac94a9
lukas <lukas>
2024-08-05 15:36:02 +0200

    Popup komponenta, custom redosljed u Table komponenti, i napravljeno da se na hover filter gumba u tablici prikaže u popup-u tekstualni filteri. (Još klik na gumb ne radi ništa)

---
+++ src/lib/General components/Popup.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let screenLocation: [number, number] = [100, 100];
+</script>
+
+<div class="popup" style="top: {screenLocation[1]}px; left: {screenLocation[0]}px;">
+  <div class="popup-content">
+    <slot>
+      <!-- Default content -->
+      <p>Default content</p>
+    </slot>
+  </div>
+</div>
+
+<style>
+  .popup {
+    position: absolute;
+    z-index: 1000;
+    background-color: rgba(0, 0, 0, 0.85);
+    color: white;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    pointer-events: none; /* Prevents the popup from interfering with mouse events */
+    transform: translate(-100%, -100%); /* Adjusts the position to be above the target */
+    width: max-content;
+    max-width: 50vw;
+    white-space: pre;
+  }
+</style>
--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,10 +1,25 @@
 <!-- Ovo je namijenjeno prvo kao generalni table ali mislim da ću specificirati za popis kupaca samo -->
 
 <script lang="ts">
-  import type { UserData } from "../types";
+  import type { Filters, UserData } from "../types";
+  import Popup from "./General components/Popup.svelte";
 
   export let showHeader: boolean = true;
   export let data: UserData[] = [];
+
+  type ParsedUserData = ReturnType<typeof parseData>[0];
+
+  let columnOrder: (keyof ParsedUserData)[] = [
+    "id",
+    "name",
+    "email",
+    "phone",
+    "note",
+    "created",
+    "updated",
+    "favoriteProperties",
+    "filters",
+  ];
 
   function parseData(data: UserData[]) {
     return data.map((user) => {
@@ -17,14 +32,59 @@
       };
     });
   }
+
+  let popupLocation: [number, number] = [0, 0];
+  let popupContent: string = "";
+  function showFiltersPopup(userId: UserData["id"], event: MouseEvent) {
+    if (!event.target) return;
+
+    console.log(event);
+
+    const targetButton = event.target as HTMLButtonElement;
+
+    const buttonLocation = targetButton.getBoundingClientRect();
+
+    // set the popupLocation to the top-center of the button
+    popupLocation = [
+      // buttonLocation.left + buttonLocation.width / 2,
+      buttonLocation.right,
+      buttonLocation.top - 10,
+    ];
+
+    const userFilters: Filters | null =
+      data.find((user) => user.id === userId)?.filters || null;
+
+    if (userFilters) {
+      const removedPolygonValues = userFilters.polygons.length;
+
+      popupContent = JSON.stringify(
+        {
+          ...userFilters,
+          polygons: removedPolygonValues,
+        },
+        null,
+        1
+      );
+    } else {
+      popupContent = "Nema filtera";
+    }
+  }
+  function hideFiltersPopup() {
+    popupLocation = [0, 0];
+    popupContent = "";
+  }
 </script>
+
+<Popup screenLocation={popupLocation}>
+  {popupContent}
+</Popup>
 
 <section class="table-container">
   <table>
     {#if showHeader && data.length > 0}
       <thead>
         <tr>
-          {#each Object.keys(parseData(data)[0]) as header}
+          {#each columnOrder as header}
             <th>{header}</th>
           {/each}
         </tr>
@@ -34,23 +94,32 @@
     <tbody>
       {#each parseData(data) as user}
         <tr>
-          {#each Object.entries(user) as [key, value]}
-            {#if typeof value === "object"}
-              {#if value.length !== undefined}
-                {#if value.length > 0}
-                  <td>{value.join(", ")}</td>
+          {#each columnOrder as key}
+            {#if typeof user[key] === "object"}
+              {#if user[key].length !== undefined}
+                {#if user[key].length > 0}
+                  <td>{user[key].join(", ")}</td>
                 {:else}
                   <td class="empty">N/A</td>
                 {/if}
               {:else}
-                <td>{JSON.stringify(value, null, 1)}</td>
+                <!-- <td>{JSON.stringify(user[key], null, 1)}</td> -->
+                <td>
+                  <button
+                    class="applyFiltersButton"
+                    on:mouseenter={(e) => {
+                      showFiltersPopup(user.id, e);
+                    }}
+                    on:mouseleave={hideFiltersPopup}>Primjeni</button
+                  >
+                </td>
               {/if}
-            {:else if value === ""}
+            {:else if user[key] === ""}
               <td class="empty">N/A</td>
             {:else if ["email", "phone"].includes(key)}
-              <td class="copyable">{value}</td>
+              <td class="copyable">{user[key]}</td>
             {:else}
-              <td>{value}</td>
+              <td>{user[key]}</td>
             {/if}
           {/each}
         </tr>
@@ -105,5 +174,21 @@
 
   .empty {
     color: hsl(0, 0%, 50%);
+  }
+
+  button.applyFiltersButton {
+    padding: 0.5rem 0.75rem;
+    background-color: hsla(216, 90%, 45%, 0.1);
+    border-radius: 0.25rem;
+    box-shadow: 0 2px 1px rgba(0, 0, 0, 0.075);
+
+    font-size: 0.875rem;
+    font-weight: 600;
+    color: #0b5eda;
+
+    transition: background-color 200ms ease-out;
+  }
+  button.applyFiltersButton:hover {
+    background-color: hsla(216, 90%, 45%, 0.2);
   }
 </style>
