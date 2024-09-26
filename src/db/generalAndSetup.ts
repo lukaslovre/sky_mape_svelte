4cf1b537f5987be70047bcdf7703e5ebb496a897
Lukas <lovretic2002@gmail.com>
2024-09-26 19:10:13 +0200

    change pocketbase url

--- src/db/generalAndSetup.ts
+++ src/db/generalAndSetup.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 
 // export const pocketbaseUrl = "http://49.13.64.0:9992";
-export const pocketbaseUrl = "https://map.arhocoaches.com";
+export const pocketbaseUrl = "https://map.arhocoaches.com/pocketbase";
 
 export const pb = new PocketBase(pocketbaseUrl);
 
