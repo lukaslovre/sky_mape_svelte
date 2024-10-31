522ffa88c37ac3c51d4e685592c89fc1d87755ae
Lukas <lovretic2002@gmail.com>
2024-10-31 11:46:03 +0100

    Refactor selectedSidebarCategory to be a derived value based on selectedPropertyIds

--- src/stores/uiStateStore.ts
+++ src/stores/uiStateStore.ts
@@ -1,15 +1,16 @@
-import { writable, type Writable } from "svelte/store";
+import { derived, writable, type Readable, type Writable } from "svelte/store";
+import { selectedPropertyIds } from "./store";
 
 type SidebarCategory = "filters" | "selectedProperty";
 
 // let initialSidebarCategory: SidebarCategory = "filters";
 let initialSidebarCategory: SidebarCategory = "selectedProperty";
 
-export const selectedSidebarCategory: Writable<SidebarCategory> =
-  writable(initialSidebarCategory);
+// selectedSidebarCategory should actually be a derived value that depends on selectedPropertyIds
+export const selectedSidebarCategory: Readable<SidebarCategory> = derived(
+  selectedPropertyIds,
+  ($selectedPropertyIds) =>
+    $selectedPropertyIds.length > 0 ? "selectedProperty" : "filters"
+);
 
 // Actions
-
-export function setSidebarCategory(category: SidebarCategory) {
-  selectedSidebarCategory.set(category);
-}
