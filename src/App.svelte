1bb13156393e1f82b4a891457e7e84b758cbff75
Lukas <lovretic2002@gmail.com>
2025-02-02 12:55:31 +0100

    Remove SideNote component, fit-width  komponenta & fix scrollbar causing overflow

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,17 @@
 <script lang="ts">
   import Header from "./components/header/Header.svelte";
-  import SideNote from "./components/common/SideNote.svelte";
   import Map from "./components/map/Map.svelte";
   import PropertyPage from "./components/properties/PropertyPage.svelte";
   import ClientsPage from "./components/clients/ClientsPage.svelte";
   import OwnersPage from "./components/owners/OwnersPage.svelte";
   import TabMenu from "./components/header/TabMenu.svelte";
   import { uiStateStore } from "./stores/uiStateStore.svelte";
-
-  // SideNote
-  type SideNoteParams = {
-    title: string;
-    value: string;
-  };
-  let openSideNote: SideNoteParams | null = $state(null);
-  function setSideNote(params: SideNoteParams) {
-    openSideNote = params;
-  }
 </script>
 
 <main>
   <Header />
 
   <div class="content">
-    <SideNote
-      isOpen={openSideNote !== null}
-      close={() => {
-        openSideNote = null;
-      }}
-      title={openSideNote?.title || ""}
-      value={openSideNote?.value || ""}
-    />
-
     <TabMenu />
 
     {#if uiStateStore.activeTab === "Map"}
@@ -49,15 +29,13 @@
 <style>
   main {
     height: 100vh;
-
     display: flex;
-    /* flex-direction: column; */
   }
 
   .content {
     height: 100vh;
     width: 100%;
-    overflow: scroll;
+    overflow-y: auto;
 
     position: relative;
   }
--- src/components/common/Close.svelte
+++ src/components/common/Close.svelte
@@ -22,10 +22,19 @@
     display: flex;
     align-items: center;
     gap: 0.25rem;
+
+    width: fit-content;
+    padding: 0.25rem 0.75rem 0.25rem 0;
+    border-radius: 0.25rem;
+    background-color: hsla(216, 90%, 45%, 0);
+    transition: background-color 150ms ease-out;
+  }
+  .close-button:hover {
+    background-color: hsla(216, 90%, 45%, 0.1);
   }
   .close-button span {
     font-size: 0.875rem;
     font-weight: 700;
-    color: #0b5eda;
+    color: hsl(216, 90%, 45%);
   }
 </style>
