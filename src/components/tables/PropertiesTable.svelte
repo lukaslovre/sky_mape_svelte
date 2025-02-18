abcec1d43218c79b62d9f78a9ff8760887c06025
Lukas <lovretic2002@gmail.com>
2025-02-18 16:52:14 +0100

    feat: add pagination and navigation controls to PropertiesTable

--- src/components/tables/PropertiesTable.svelte
+++ src/components/tables/PropertiesTable.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import ChevronIcon from "../../assets/icons/ChevronIcon.svelte";
   import type { Property } from "../../types";
   import TableHeader from "./general/TableHeader.svelte";
   import PropertiesTableBody from "./PropertiesTableBody.svelte";
@@ -19,7 +20,14 @@
     sortOptionField,
   }: Props = $props();
 
-  // State Variables
+  // Navigation
+  const rowsPerPage = 50;
+  let startingRow: number = $state(0);
+  let totalRows: number = $derived(data.length);
+  let currentPage: number = $derived(Math.ceil(startingRow / rowsPerPage) + 1);
+  let totalPages: number = $derived(Math.ceil(totalRows / rowsPerPage));
+
+  // Select all checkbox
   let isSelectAll = $derived(selectedPropertyIds.length === data.length);
 
   type Column = {
@@ -159,40 +167,72 @@
       )
       .filter(Boolean) as string[];
   }
+
+  function handleTableNavigationClick(direction: "previous" | "next") {
+    if (direction === "previous") {
+      startingRow -= rowsPerPage;
+    } else if (direction === "next") {
+      startingRow += rowsPerPage;
+    }
+  }
 </script>
 
-<section class="table-container">
-  <table>
-    {#if showHeader}
-      <TableHeader
-        {toggleSelectAll}
-        {isSelectAll}
-        columnsLabels={columns
-          .filter((column) => column.visible)
-          .map((column) => column.label)}
-        {getSelectedAttributes}
-        copyableColumnsLabels={columns
-          .filter((column) => column.copyable)
-          .map((column) => column.label)}
-        sortOptionLabel={columns.find((column) => column.key === sortOptionField)?.label}
-      />
-    {/if}
+<section class="table-container-wrapper">
+  <section class="table-container">
+    <table>
+      {#if showHeader}
+        <TableHeader
+          {toggleSelectAll}
+          {isSelectAll}
+          columnsLabels={columns
+            .filter((column) => column.visible)
+            .map((column) => column.label)}
+          {getSelectedAttributes}
+          copyableColumnsLabels={columns
+            .filter((column) => column.copyable)
+            .map((column) => column.label)}
+          sortOptionLabel={columns.find((column) => column.key === sortOptionField)
+            ?.label}
+        />
+      {/if}
 
-    {#if data.length > 0}
-      <PropertiesTableBody
-        {selectedPropertyIds}
-        {data}
-        columnsKeys={columns
-          .filter((column) => column.visible)
-          .map((column) => column.key)}
-        {handleCheckboxClick}
-        copyableColumnsKeys={columns
-          .filter((column) => column.copyable)
-          .map((column) => column.key)}
-        {sortOptionField}
-      />
-    {/if}
-  </table>
+      {#if data.length > 0}
+        <PropertiesTableBody
+          {selectedPropertyIds}
+          data={data.slice(startingRow, startingRow + rowsPerPage)}
+          columnsKeys={columns
+            .filter((column) => column.visible)
+            .map((column) => column.key)}
+          {handleCheckboxClick}
+          copyableColumnsKeys={columns
+            .filter((column) => column.copyable)
+            .map((column) => column.key)}
+          {sortOptionField}
+        />
+      {/if}
+    </table>
+  </section>
+
+  <!-- Table navigation -->
+  <div class="table-navigation-container">
+    <p>{startingRow}-{startingRow + rowsPerPage} od {totalRows}</p>
+
+    <div class="table-navigation-buttons">
+      <button
+        onclick={() => handleTableNavigationClick("previous")}
+        disabled={startingRow === 0}
+      >
+        <ChevronIcon color="hsl(0, 0%, 30%)" rotation={180} size={24} />
+      </button>
+      <span>{currentPage}/{totalPages}</span>
+      <button
+        onclick={() => handleTableNavigationClick("next")}
+        disabled={startingRow + rowsPerPage >= totalRows}
+      >
+        <ChevronIcon color="hsl(0, 0%, 30%)" rotation={0} size={24} />
+      </button>
+    </div>
+  </div>
 </section>
 
 <style>
@@ -204,5 +244,43 @@
     border-collapse: collapse;
     width: max-content;
     table-layout: fixed;
+  }
+
+  .table-navigation-container {
+    margin-top: 1rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-size: 0.875rem;
+    color: hsl(0, 0%, 40%);
+  }
+
+  .table-navigation-buttons {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  .table-navigation-buttons button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0.25rem;
+
+    background-color: transparent;
+    border: 1px solid hsl(0, 0%, 80%);
+    border-radius: 0.25rem;
+
+    cursor: pointer;
+    transition: background-color 150ms ease-out;
+  }
+  .table-navigation-buttons button:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+  .table-navigation-buttons button:hover {
+    background-color: hsl(0, 0%, 90%);
   }
 </style>
